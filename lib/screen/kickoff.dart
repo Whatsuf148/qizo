f880b6686f8028f25d537274e46cf3ab47291ff3
Whatsuf148 <whatsufsachin@gmail.com>
2023-01-11 19:58:29 +0545

    LogiIn Page Modification

--- lib/screen/kickoff.dart
+++ lib/screen/kickoff.dart
@@ -8,29 +8,28 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset(
-          "photo/kickoff.jpg",
-          height: 350,
-          width: 350,
-        ),
-        const Text(
-          " Create Share & Play\nQuizzes Whenever and\nWhereever you want",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            wordSpacing: sqrt1_2,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "photo/kickoff.jpg",
+            height: 350,
+            width: 350,
           ),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: InkWell(
-            onTap: () {},
+          const Text(
+            " Create Share & Play\nQuizzes Whenever and\nWhereever you want",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              wordSpacing: sqrt1_2,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
             child: Container(
               height: 50,
               width: double.infinity,
@@ -38,51 +37,56 @@ class StartPage extends StatelessWidget {
                 color: const Color(0xff6949FF),
                 borderRadius: BorderRadius.circular(50),
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "GET STARTED",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    "GET STARTED",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: ((context) => const LogInPage())));
-            },
-            child: Container(
-              height: 50,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color(0xffF0EDFF),
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "I ALREADY HAVE AN ACCOUNT",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xff6949FF),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: ((context) => const LogInPage())));
+              },
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0xffF0EDFF),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "I ALREADY HAVE AN ACCOUNT",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xff6949FF),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,54 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:qizo/screen/kickoff.dart';
 
-class LogInPage extends StatelessWidget {
+class LogInPage extends StatefulWidget {
   const LogInPage({super.key});
 
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  bool check = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffE5E5E5),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const StartPage()));
+                    },
+                    icon: const Icon(Icons.arrow_back))),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Hello There",
+                    style: TextStyle(
+                      fontSize: 33,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Align(
               alignment: Alignment.topLeft,
-              child: IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.arrow_back))),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: const [
-              Padding(
+              child: Padding(
                 padding: EdgeInsets.all(8.0),
                 child: Text(
-                  "Hello There",
+                  "Email",
                   style: TextStyle(
-                    fontSize: 33,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "Email",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          const TextField()
-        ],
+            const TextField(),
+            const SizedBox(
+              height: 40,
+            ),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Password",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: check,
+                  onChanged: ((value) {
+                    setState(() {
+                      check = !check;
+                    });
+                  }),
+                  activeColor: const Color(0XFF6E50FF),
+                ),
+                const Text(
+                  "Remember Me",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Color(0xff745BE7),
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            const SizedBox(
+              height: 100,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: const Color(0xff745BE7),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(14.0),
+                          child: Text(
+                            "SIGN IN ",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
