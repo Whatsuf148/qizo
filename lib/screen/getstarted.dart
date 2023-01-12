fe4ce5eb1e43d5bfef3b1dc5a3a84e128a8ad1f1
Mohammad Mustafa <mustafa@sherpa-tech.com>
2023-01-12 21:27:28 +0545

    list added

--- lib/screen/getstarted.dart
+++ lib/screen/getstarted.dart
@@ -7,6 +7,23 @@ class GetStartedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> contents = [
+      {
+        "title": "",
+        "icon": Icons.abc,
+        "color": Colors.red
+      },
+      {
+        "title": "",
+        "icon": "",
+        "color": Colors.red
+      },
+      {
+        "title": "",
+        "icon": Icons.abc,
+        "color": Colors.red
+      },
+    ];
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -32,18 +49,11 @@ class GetStartedPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "What type of account do you \nlike to create?" "",
+                "What type of account do you \nlike to create? 👦🏻",
                 textAlign: TextAlign.center,
                 style: GoogleFonts.poppins(
                   fontSize: 24,
                   fontWeight: FontWeight.w600,
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Image.asset(
-                  'photo/boyemoji.jpg',
-                  height: 24,
                 ),
               ),
             ],
